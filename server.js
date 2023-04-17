a25cdaaf565ada15fc5de07fe051c9fdd9ea4160
Fredrick Muikia <thuofred1@gmail.com>
2023-04-17 01:14:17 -0500

    heroku config

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/auth");
 
+// Accessing the path module
+const path = require("path");
+
 dotenv.config();
 const app = express();
 
@@ -27,5 +30,14 @@ app.get("/", (req, res) => {
 // Use authentication routes
 app.use("/api/auth", authRoutes);
 
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+
