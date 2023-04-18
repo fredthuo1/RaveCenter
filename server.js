197c44f51abe4dc2c0e96dcbbdebdf1bd3e4b032
Fredrick Muikia <thuofred1@gmail.com>
2023-04-18 02:17:28 -0500

    Serve client build files

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const authRoutes = require("./routes/auth");
 
 // Accessing the path module
 const authRoutes = require('./routes/auth');
