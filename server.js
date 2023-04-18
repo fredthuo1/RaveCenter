c94166d2ff150f1a887965bbf27ce854b954a634
Fredrick Muikia <thuofred1@gmail.com>
2023-04-18 01:52:15 -0500

    Prepare for Heroku deployment

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const dotenv = require("dotenv");
 const authRoutes = require("./routes/auth");
 
 // Accessing the path module
-const path = require("path");
+const authRoutes = require('./routes/auth');
 
 dotenv.config();
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -25,15 +26,18 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 // Use authentication routes
 app.use("/api/auth", authRoutes);
 
-// Step 1:
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-// The "catchall" handler: for any request that doesn't
-// match one of the above, send back the index.html file.
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+// Serve static assets (React build) in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-
 
