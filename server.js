de634e0c421516ffa9637a8527fae8d92df11ba0
Fredrick Muikia <thuofred1@gmail.com>
2023-04-18 02:37:59 -0500

    config changes

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
+// ...
+const path = require('path');
+
 // Accessing the path module
 const authRoutes = require('./routes/auth');
 
