504fa6519df4037e6df06c41626aceb8afefe9e3
Fredrick Muikia <thuofred1@gmail.com>
2023-04-18 02:46:02 -0500

    minor fixes

--- server.js
+++ server.js
@@ -30,10 +30,10 @@ app.use("/api/auth", authRoutes);
 
 // Serve static assets (React build) in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
   });
 }
 
